1945ae543296c2b093d2db7fda26aaadc7cca4c6
K4pocha <k4pocha@gmail.com>
2026-05-16 22:04:52 -0400

    inicio proyecto ionic standalone + reestructuracion carpetas (eliminacion tabs)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,4 +5,16 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'recorder',
+    loadComponent: () => import('./pages/recorder/recorder.page').then( m => m.RecorderPage)
+  },
+  {
+    path: 'history',
+    loadComponent: () => import('./pages/history/history.page').then( m => m.HistoryPage)
+  },
 ];
--- frontend/src/app/tabs/tabs.routes.ts
+++ frontend/src/app/tabs/tabs.routes.ts
@@ -7,30 +7,27 @@ export const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadComponent: () =>
-          import('../tab1/tab1.page').then((m) => m.Tab1Page),
+        path: 'home',
+        loadComponent: () => import('../pages/home/home.page').then(m => m.HomePage)
       },
       {
-        path: 'tab2',
-        loadComponent: () =>
-          import('../tab2/tab2.page').then((m) => m.Tab2Page),
+        path: 'recorder',
+        loadComponent: () => import('../pages/recorder/recorder.page').then(m => m.RecorderPage)
       },
       {
-        path: 'tab3',
-        loadComponent: () =>
-          import('../tab3/tab3.page').then((m) => m.Tab3Page),
+        path: 'history',
+        loadComponent: () => import('../pages/history/history.page').then(m => m.HistoryPage)
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
-        pathMatch: 'full',
-      },
-    ],
+        redirectTo: '/tabs/home',
+        pathMatch: 'full'
+      }
+    ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
-    pathMatch: 'full',
-  },
+    redirectTo: '/tabs/home',
+    pathMatch: 'full'
+  }
 ];
