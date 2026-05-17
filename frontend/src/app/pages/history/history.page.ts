efd3dfb59d5e5c8357d3d24cfc9199c633d903b2
K4pocha <k4pocha@gmail.com>
2026-05-17 14:23:31 -0400

    home fix ui

--- frontend/src/app/pages/history/history.page.ts
+++ frontend/src/app/pages/history/history.page.ts
@@ -1,20 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import { 
+  IonContent, IonIcon, IonButton, IonBadge 
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { 
+  timeOutline, eyeOutline, micOutline, arrowForwardOutline, 
+  videocamOutline, calendarOutline, chevronForwardOutline 
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-history',
   templateUrl: './history.page.html',
   styleUrls: ['./history.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule, FormsModule, RouterLink,
+    IonContent, IonIcon, IonButton, IonBadge
+  ]
 })
 export class HistoryPage implements OnInit {
 
-  constructor() { }
+  // Listado cronológico de ensayos previos del usuario
+  listaEnsayos = [
+    {
+      id: 'p-03',
+      titulo: 'Pitch Postulación VIU - Versión Final',
+      fecha: '17 Mayo, 2026',
+      rubrica: 'VIU ANID',
+      scoreGlobal: 76,
+      scoreNoVerbal: 82,
+      scoreVerbal: 69
+    },
+    {
+      id: 'p-02',
+      titulo: 'Ensayo Ajuste de Tiempo y Oratoria',
+      fecha: '14 Mayo, 2026',
+      rubrica: 'VIU ANID',
+      scoreGlobal: 68,
+      scoreNoVerbal: 70,
+      scoreVerbal: 65
+    },
+    {
+      id: 'p-01',
+      titulo: 'Primer Intento - Estructura Básica',
+      fecha: '08 Mayo, 2026',
+      rubrica: 'VIU ANID',
+      scoreGlobal: 52,
+      scoreNoVerbal: 55,
+      scoreVerbal: 48
+    }
+  ];
 
-  ngOnInit() {
+  constructor() {
+    addIcons({
+      timeOutline, eyeOutline, micOutline, arrowForwardOutline,
+      videocamOutline, calendarOutline, chevronForwardOutline
+    });
   }
 
+  ngOnInit() {}
 }
